7345f53f6236772e6282d56e14ef14b08ca47a12
N <weitch-96-96-96@mail.ru>
2023-07-11 14:03:59 +0300

    Задание по Массивам готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let us = users.filter(item => item.gender === gender).map(item => item.age);
+	let result = us.reduce((sum, age) => sum + age, 0);
+
+	return (us.length > 0) ? result / us.length : 0;
 }
